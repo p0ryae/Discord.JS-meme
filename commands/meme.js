cb7d424ff92d65036a20cdfd3b99869bd52e3141
ElCholoGamer <josedanielgrayson@gmail.com>
2020-12-11 19:33:33 -0500

    These can be const I guess

--- commands/meme.js
+++ commands/meme.js
@@ -2,25 +2,29 @@ const Discord = require('discord.js');
 const got = require('got');
 
 module.exports.run = async (bot, message, args) => {
-    const embed = new Discord.MessageEmbed();
-    got('https://www.reddit.com/r/memes/random/.json').then(response => {
-        let content = JSON.parse(response.body);
-        let permalink = content[0].data.children[0].data.permalink;
-        let memeUrl = `https://reddit.com${permalink}`;
-        let memeImage = content[0].data.children[0].data.url;
-        let memeTitle = content[0].data.children[0].data.title;
-        let memeUpvotes = content[0].data.children[0].data.ups;
-        let memeNumComments = content[0].data.children[0].data.num_comments;
-        embed.setTitle(`${memeTitle}`);
-        embed.setURL(`${memeUrl}`)
-        embed.setColor('RANDOM')
-        embed.setImage(memeImage);
-        embed.setFooter(`👍 ${memeUpvotes} 💬 ${memeNumComments}`);
-        message.channel.send(embed)
-    }).catch(console.error);
-}
+	const embed = new Discord.MessageEmbed();
+	got('https://www.reddit.com/r/memes/random/.json')
+		.then(response => {
+			const content = JSON.parse(response.body);
+			const permalink = content[0].data.children[0].data.permalink;
+			const memeUrl = `https://reddit.com${permalink}`;
+			const memeImage = content[0].data.children[0].data.url;
+			const memeTitle = content[0].data.children[0].data.title;
+			const memeUpvotes = content[0].data.children[0].data.ups;
+			const memeNumComments = content[0].data.children[0].data.num_comments;
+
+			embed.setTitle(`${memeTitle}`);
+			embed.setURL(`${memeUrl}`);
+			embed.setColor('RANDOM');
+			embed.setImage(memeImage);
+			embed.setFooter(`👍 ${memeUpvotes} 💬 ${memeNumComments}`);
+
+			message.channel.send(embed);
+		})
+		.catch(console.error);
+};
 
 module.exports.config = {
-    name: "meme",
-    aliases: []
-}
+	name: 'meme',
+	aliases: [],
+};
