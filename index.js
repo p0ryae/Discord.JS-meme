c3c6b992ab0beaff73d7b59699517a4f2e8dd69b
ElCholoGamer <josedanielgrayson@gmail.com>
2020-12-11 19:24:59 -0500

    Change some indent and stuff
    - Fix an indent thing
    - Add regex to split the message

--- index.js
+++ index.js
@@ -11,16 +11,17 @@ client.aliases = new Discord.Collection();
 
 loadCommands(client);
 
-client.on('message', async (message) => {
+client.on('message', message => {
     if (message.author.bot) return;
 
-	const messageArray = message.content.split(' ');
-	const cmd = messageArray[0];
+    const messageArray = message.content.split(/\s+/); // This regex splits on every whitespace
+    const cmd = messageArray[0];
     const args = messageArray.slice(1);
     
     const prefix = "!";
 
-	if (!message.content.startsWith(prefix)) return;
+    if (!message.content.startsWith(prefix)) return;
+    
     const commandfile = client.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)));
     commandfile.run(client, message, args);
-})
+});
