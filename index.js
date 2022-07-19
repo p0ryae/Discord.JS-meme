26fa75e9c42b58bb1decf137d8efeb7b10f8f98e
tryka-1213 <107329072+tryka-1213@users.noreply.github.com>
2022-07-19 18:39:13 +0100

    DJS v14 changes
    - Partials changed to adhere to new syntax
    - (necessary) intents added
    - Previous intent bit value of 32767 removed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const Discord = require('discord.js');
-const client = new Discord.Client({
-    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
-    intents: 32767,
+const { Discord, Client, Partials, GatewayIntentBits } = require('discord.js');
+const client = new Client({
+    partials: [Partials.Message, Partials.Channel, Partials.Reaction],
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers],
 });
 const { loadCommands } = require('./utils/loadCommands');
 
